e9e4ae9073c0f70ef489050b87aed4380df1732f
vivek-Ravikumar <52356462+vivek-Ravikumar@users.noreply.github.com>
2020-02-02 10:39:04 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-var http = require("http");
+const express = require("express");
+const students = require("./models/student");
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.get("/students", (req, res) => {
+  // res.status(200);
+  // res.send(students);
+
+  res.status(200).json({ students });
+});
+
+const server = app.listen(3000, (req, res) => {
+  console.log(`server running on port : ${server.address().port}`);
+});
---
+++ src/models/student.js
@@ -0,0 +1,25 @@
+const students = [
+  {
+    id: 1,
+    FirstName: "vijay",
+    lastName: "vel",
+    age: 24,
+    gender: "male"
+  },
+  {
+    id: 2,
+    FirstName: "vimal",
+    lastName: "v",
+    age: 24,
+    gender: "male"
+  },
+  {
+    id: 3,
+    FirstName: "Rohit",
+    lastName: "J",
+    age: 24,
+    gender: "male"
+  }
+];
+
+module.exports = students;
